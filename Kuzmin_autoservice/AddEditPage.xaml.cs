c6f03f5b697b81b82194b26597f39dc78d811b90
V <V@DESKTOP-PTCRLDS>
2024-12-03 10:17:30 +0500

    Добавьте файлы проекта.

---
+++ Kuzmin_autoservice/AddEditPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kuzmin_autoservice
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Service _currentService = new Service();
+        public AddEditPage(Service SelectedSercice)
+        {
+            InitializeComponent();
+            if  (SelectedSercice != null)
+                _currentService = SelectedSercice;
+            DataContext = _currentService;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentService.Title))
+                errors.AppendLine("Укажите название услуги");
+
+            if (_currentService.Cost == 0)
+                errors.AppendLine("Укажите стоимость услуги");
+
+            if (_currentService.DiscountInt < 0 || _currentService.DiscountInt > 100)
+                errors.AppendLine("Укажите скидку от 0 до 100");
+
+            if (_currentService.DurationInSeconds <= 0)
+                errors.AppendLine("Укажите длительность услуги");
+                
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+                if (_currentService.ID == 0)
+                {
+                    Kuzmin_autoserviceEntities.GetContext().Service.Add(_currentService);
+                }
+                try
+                {
+                    Kuzmin_autoserviceEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Информация сохранена");
+                    Manager.MainFrame.GoBack();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+
+    }
+}
